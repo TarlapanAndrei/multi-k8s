cc5a6ec80fee3b2c954980efd0b90df3fc6430c5
Andrei Tarlapan <tarlapan@gmail.com>
2021-02-26 20:50:41 +0200

    debag Dockerrun.aws.json

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and more
+          Learn React
         </a>
       </header>
       <div>
